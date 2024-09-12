d19df1ce44bcaca8d88d8cc6ddcdb85e1b0f3d42
f-azumah <135899899+f-azumah@users.noreply.github.com>
2024-09-12 15:51:02 -0400

    Update interp-ce.rkt

--- interp-ce.rkt
+++ interp-ce.rkt
@@ -5,34 +5,8 @@
 
 (provide interp-ce)
 
-; Your task is to write a CE interpreter for a substantial subset of Scheme/Racket. 
-; A CE interpreter is meta-circular to a large degree (e.g., a conditional in the target
-; language (scheme-ir?) can be implemented using a conditional in the host language (Racket),
-; recursive evaluation of a sub-expression can be implemented as a recursive call to the
-; interpreter, however, it's characterized by creating an explicit closure value for lambdas
-; that saves its static environment (the environment when it's defined). For example, a CE
-; interpreter for the lambda calculus may be defined:
-(define (interp-ce-lambda exp [env (hash)])
-  (match exp
-         [`(lambda (,x) ,body)
-          ; Return a closure that pairs the code and current (definition) environment
-          `(closure (lambda (,x) ,body) ,env)]
-         [`(,efun ,earg)
-          ; Evaluate both sub-expressions
-          (define vfun (interp-ce-lambda efun env))  
-          (define varg (interp-ce-lambda earg env))
-          ; the applied function must be a closure
-          (match-define `(closure (lambda (,x) ,body) ,env+) vfun)
-          ; we extend the *closure's environment* and interp the body
-          (interp-ce-lambda body (hash-set env+ x varg))]
-         [(? symbol? x)
-          ; Look up a variable in the current environment
-          (hash-ref env x)]))
-
-; Following is a predicate for the target language you must support. You must support any
-; syntax allowed by scheme-ir that runs without error in Racket, returning a correct value..
+; Following is a predicate for the my language.
 (define (scheme-ir? exp)
-  ; You should support a few built-in functions bound to the following variables at the top-level
   (define (prim? s) (member s '(+ - * = equal? list cons car cdr null?)))
   (match exp
          [`(lambda ,(? (listof symbol?)) ,(? scheme-ir?)) #t] ; fixed arguments lambda
@@ -51,11 +25,9 @@
          [''() #t]
          [_ #f]))
 
-; Interp-ce must correctly interpret any valid scheme-ir program and yield the same value
-; as DrRacket, except for closures which must be represented as `(closure ,lambda ,environment).
-; (+ 1 2) can return 3 and (cons 1 (cons 2 '())) can yield '(1 2). For programs that result in a 
-; runtime error, you should return `(error ,message)---giving some reasonable string error message.
-; Handling errors and some trickier cases will give bonus points. 
+; Interp-ce interprets any valid scheme-ir program and yields the same value
+; as DrRacket, except for closures which are represented as `(closure ,lambda ,environment).
+
 (define (interp-ce exp)
   ; Might add helpers or other code here...             
   (define (interp exp env) 
@@ -63,7 +35,6 @@
       [(? symbol? x) (hash-ref env x)]
       
       [`(lambda ,args ,body) 
-       ;; closures must be represented this way
        `(closure ,exp ,env)]
       
       [`(lambda (,xs ...) ,e)
@@ -155,9 +126,7 @@
          [(? procedure? p)
           (apply p (map (lambda (e) (interp e env)) eargs))])]))
         
-  ;; you need to cook up a starting environment: at first it can just
-  ;; be the empty hash, but later on you may want to add things like
-  ;; builtins here.
+  ;; starting environment: 
   (define starting-env 
     (hash '+ +
           '- -
